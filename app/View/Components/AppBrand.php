74a44daa1e07dcfbb86e20cb0c0d0629ed1ffaef
Rob Pungello <rob.pungello@gmail.com>
2025-06-26 18:28:25 -0400

    Display app name in brand icon

--- app/View/Components/AppBrand.php
+++ app/View/Components/AppBrand.php
@@ -19,25 +19,10 @@ class AppBrand extends Component
     /**
      * Get the view / contents that represent the component.
      */
-    public function render(): View|Closure|string
+    public function render(): View
     {
-        return <<<'HTML'
-                <a href="/" wire:navigate>
-                    <!-- Hidden when collapsed -->
-                    <div {{ $attributes->class(["hidden-when-collapsed"]) }}>
-                        <div class="flex items-center gap-2 w-fit">
-                            <x-icon name="o-cube" class="w-6 -mb-1.5 text-purple-500" />
-                            <span class="font-bold text-3xl me-3 bg-gradient-to-r from-purple-500 to-pink-300 bg-clip-text text-transparent ">
-                                app
-                            </span>
-                        </div>
-                    </div>
-
-                    <!-- Display when collapsed -->
-                    <div class="display-when-collapsed hidden mx-5 mt-5 mb-1 h-[28px]">
-                        <x-icon name="s-cube" class="w-6 -mb-1.5 text-purple-500" />
-                    </div>
-                </a>
-            HTML;
+        return view('components.app-brand', [
+            'name' => config('app.name'),
+        ]);
     }
 }
