87d2b3613666426aaf797f9a5cc10fc06d32181f
Rob Pungello <rob.pungello@gmail.com>
2025-06-26 18:28:34 -0400

    Shorten default app name

--- config/app.php
+++ config/app.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'name' => env('APP_NAME', 'ytdl-sub Manager'),
+    'name' => env('APP_NAME', 'ytdl-sub'),
 
     /*
     |--------------------------------------------------------------------------
