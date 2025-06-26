5225895ccc08da54bde4322cd798747f4f1ad322
Rob Pungello <rob.pungello@gmail.com>
2025-06-26 16:17:44 -0400

    Add sweetalert

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,4 +1,6 @@
 import axios from 'axios';
 window.axios = axios;
-
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+
+import Swal from 'sweetalert2'
+window.Swal = Swal
