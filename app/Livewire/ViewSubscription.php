007fe6019630a0540b828a74cf0a5963b9ad6e1e
Rob Pungello <rob@rpun.io>
2025-09-19 19:27:51 -0400

    Enhanced video list

--- app/Livewire/ViewSubscription.php
+++ app/Livewire/ViewSubscription.php
@@ -4,6 +4,8 @@ namespace App\Livewire;
 
 use App\Concerns\InteractsWithSubscriptions;
 use Illuminate\Contracts\View\View;
+use Illuminate\Support\Arr;
+use Illuminate\Support\Number;
 use Livewire\Attributes\Computed;
 use Livewire\Component;
 
@@ -15,14 +17,16 @@ class ViewSubscription extends Component
 
     protected string $path;
 
+    protected string $directory;
+
     public function mount(string $key): void
     {
-        $directory = $this->getDirectoryForKey($key);
-        $this->name = basename($directory);
-        if (! $this->archiveFileExists($key, $directory)) {
+        $this->directory = $this->getDirectoryForKey($key);
+        $this->name = basename($this->directory);
+        if (! $this->archiveFileExists($key, $this->directory)) {
             abort(404);
         }
-        $this->path = $this->getArchiveFilePath($key, $directory);
+        $this->path = $this->getArchiveFilePath($key, $this->directory);
     }
 
     public function render(): View
@@ -39,7 +43,33 @@ class ViewSubscription extends Component
         );
 
         usort($videos, fn (array $a, array $b) => strcmp($b['upload_date'], $a['upload_date']));
+        return array_map(fn (array $video) => $this->mapVideo($video), $videos);
+    }
 
-        return $videos;
+    private function mapVideo(array $video): array
+    {
+        $info = $this->readInfo(
+            $this->getInfoPath($video)
+        );
+
+        return [
+            'title' => $info['title'],
+            'resolution' => $info['resolution'],
+            'upload_date' => $video['upload_date'],
+            'size' => Number::fileSize($info['filesize_approx']),
+        ];
+    }
+
+    private function readInfo(string $path): array
+    {
+        return json_decode(
+            file_get_contents("$this->directory/$path"),
+            true,
+        );
+    }
+
+    private function getInfoPath(array $video): string
+    {
+        return Arr::first($video['file_names'], fn ($file) => str_ends_with($file, '.info.json'));
     }
 }
--- resources/views/livewire/view-subscription.blade.php
+++ resources/views/livewire/view-subscription.blade.php
@@ -6,12 +6,16 @@
         <flux:table.columns>
             <flux:table.column>{{ __('Upload Date') }}</flux:table.column>
             <flux:table.column>{{ __('Title') }}</flux:table.column>
+            <flux:table.column>{{ __('Resolution') }}</flux:table.column>
+            <flux:table.column>{{ __('Size') }}</flux:table.column>
         </flux:table.columns>
         <flux:table.rows>
             @foreach($this->videos as $video)
                 <flux:table.row>
                     <flux:table.cell>{{ $video['upload_date'] }}</flux:table.cell>
-                    <flux:table.cell>{{ $video['file_names'][0] }}</flux:table.cell>
+                    <flux:table.cell>{{ $video['title'] }}</flux:table.cell>
+                    <flux:table.cell>{{ $video['resolution'] }}</flux:table.cell>
+                    <flux:table.cell>{{ $video['size'] }}</flux:table.cell>
                 </flux:table.row>
             @endforeach
         </flux:table.rows>
